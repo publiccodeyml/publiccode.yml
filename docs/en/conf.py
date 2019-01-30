129ece30a95083b1709c2e71605ede468d19427e
libremente <surf@libremente.eu>
2019-01-30 11:29:36 +0100

    fix: moving to RST format, not needed anymore

--- docs/en/conf.py
+++ docs/en/conf.py
@@ -3,13 +3,3 @@
 
 # The master toctree document.
 master_doc = 'index'
-
-# Monkey patch to fix recommonmark 0.4 doc reference issues.
-# See https://github.com/rtfd/recommonmark/issues/93#issuecomment-441798805
-from recommonmark.states import DummyStateMachine
-orig_run_role = DummyStateMachine.run_role
-def run_role(self, name, options=None, content=None):
-    if name == 'doc':
-        name = 'any'
-    return orig_run_role(self, name, options, content)
-DummyStateMachine.run_role = run_role
